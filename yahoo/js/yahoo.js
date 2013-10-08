ce676ee02c1060cfd16ce832ad863d206434144d
cslgzuozhaoyin <zeyozuo@163.com>
2013-10-08 22:59:29 +0800

    web page:yahoo(Finished) and YHOUSE(Create)
    First:
    Finishing webpage is yahoo that has done in yestoday.
    1.Function include:
    (1)In main center ,there is a slide that is difficult and meaning;
    (2)When the mouse scroll, three contents (top、left、right)need to be
    fixed position.
    Second:
    Creating a new web page that is YHOUSE.Including:
    1.Contents include:layout、top、as part of the main and foot
    2.Function include:
    (1)footTop中，鼠标移入和移出时，字体颜色的改变；
    changeFootTop();
    (2)type="text"，当文本框获得和失去焦点时，根据现有文本框的内容判断是否改变文本框的内容
    changeTextContent();
    (3)mainLeft1 slide1中，幻灯片的实现；当鼠标移入和移出图片时，停止幻灯片的转换；当鼠标放到相应的小图标时，显示相应的图片
    slide1MainLeft1();
    (4)main2Left中，改变透明度
    $(".main2LeftImgs span").css("opacity","0.5");
    (5)main2Right中，选项卡
    tabMain2Right();
    3.Especially find:
    (1)Background-color can set the transparency eg:background-color:
    rgba(255,255,255,0.5);
    (2)The contents are all float. when The width, height of parent element
    are auto, it is equal to the default value.
    4.Unfinished:
    (1)The content of main and some Special effects.

--- yahoo/js/yahoo.js
+++ yahoo/js/yahoo.js
@@ -8,6 +8,13 @@ $(function(){
 	//mainRight8中，下拉框，选择显示内容，并提交
 	selectMainRight8();
 
+	//mainCenterTop中，随着鼠标移入和移出，改变透明度，改变边框底部的颜色，以及自动交换图片和描述
+	changeOpcitymainCenterSlides();
+	//当鼠标滑动时，top、left、right定位
+	scrollAndFixed();
+	//type="text"，当文本框获得和失去焦点时，根据现有文本框的内容判断是否改变文本框的内容
+	changeTextContent();
+
 });
 
 //mainRight3中，“关闭”的显示和隐藏、边框变化及执行关闭
@@ -61,5 +68,110 @@ function selectMainRight8(){
 		$(".mainRight3TopAS").text(txt);
 		$(this).parents(".mainRight3TopRSelect").hide();
 		$(".mainRight8content1 img").attr("src","css/images/"+txt+".jpg");
+	});
+}
+//mainCenterTop中，随着鼠标移入和移出，改变透明度，改变边框底部的颜色，以及自动交换图片和描述
+var handle;
+function changeOpcitymainCenterSlides(){
+	$(".mainCenterSlidesTopDes").hover(function(){
+		$(this).css("opacity","1");
+	},function(){
+		$(this).css("opacity","0.7");
+	});
+
+	$(".mainCenterSlidesBottom li").hover(function(){
+		$(".mainCenterSlidesBottom li").css("border-bottom","5px solid #fff");
+		$(this).css("border-bottom","5px solid rgb(80, 0, 149)");
+		var val=parseInt($(this).attr("value"));
+		clearInterval(handle);
+		indexSlides=val+1;
+		$(".mainCenterSlidesTop img").attr("src","css/images/11"+val+".jpg");
+		changeDes(val);
+
+	},function(){
+		$(this).css("border-bottom","#fff");
+		handle=setInterval("slidesFun()",1000);
+	});
+
+	$(".mainCenterSlidesTop").hover(function(){
+		clearInterval(handle);
+	},function(){
+		handle=setInterval("slidesFun()",1000);
+	});
+	handle=setInterval("slidesFun()",1000);
+}
+//幻灯片实现代码
+var indexSlides=1;
+function slidesFun(){
+	if (indexSlides==6) {
+		indexSlides=1;
+	};
+	$(".mainCenterSlidesBottom li").css("border-bottom","5px solid #fff");
+	$(".mainCenterSlidesBottom li:nth-child("+indexSlides+")").css("border-bottom","5px solid rgb(80, 0, 149)");
+	$(".mainCenterSlidesTop img").attr("src","css/images/11"+indexSlides+".jpg");
+	changeDes(indexSlides);
+	indexSlides++;
+}
+//slide中描述的改变
+function changeDes(i){
+	var h3,p,pa;
+	switch(i){
+		case 2:h3="Bryan Cranston's 'transformative' new role";
+				p="The actor portrays another imposing figure, but it's a huge departure from his gritty turn in Breaking";
+				break;
+		case 3:h3="Hugh Jackman turns the tables on interviewer";
+				p="The star instantly recognizes a young man on the red carpet from his past — and the Q&A takes a turn.";
+				break;
+		case 4:h3="Bryan Cranston's 'transformative' new role";
+				p="The actor portrays another imposing figure, but it's a huge departure from his gritty turn in Breaking";
+				break;
+		case 5:h3="Hugh Jackman turns the tables on interviewer";
+				p="The actor figure, but it's a huge departure from his gritty turn in Breaking";
+				break;
+		default:h3="Most expensive homes on the U.S. market";
+				p="If you can't afford $115M to buy this Beverly Hills stunner, you could rent it — for $600K a month.";
+	}
+	$(".mainCenterSlidesTopDesH3 a").text(h3);
+	$(".mainCenterSlidesTopDesMain p").text(p);
+}
+
+//当鼠标滑动时，top、left、right定位
+function scrollAndFixed(){
+	$(window).scroll(function(){
+		var scr=$(window).scrollTop();
+		if (scr>=88) {
+			$("#top").css({"position":"fixed","top":"0","z-index":"99","box-shadow":"rgb(80, 0, 149) 0px 0px 9px 0px"});
+			
+		}else{
+			$("#top").css({"position":"static","box-shadow":"none"});
+		};
+		if (scr>=100) {
+			$("#mainLeftMain").css({"position":"fixed","top":"-50px","z-index":"98"});
+		}else{
+			$("#mainLeftMain").css({"position":"static"});
+		};
+		if (scr>=1550) {
+			$("#mainRightMain").css({"position":"fixed","top":"-1550px","z-index":"98"});
+		}else{
+			$("#mainRightMain").css({"position":"static"});
+		};
+	});
+}
+
+//type="text"，当文本框获得和失去焦点时，根据现有文本框的内容判断是否改变文本框的内容
+function changeTextContent(){
+	$("[type='text']").focus(function(){
+		//alert("d");
+		var val=$(this).val();
+		var txt=$(this).attr("name");
+		if (val==txt) {
+			$(this).val("");
+		};
+	}).blur(function(){
+		var val=$(this).val();
+		var txt=$(this).attr("name");
+		if (val=="") {
+			$(this).val(txt);
+		};
 	});
 }
---
+++ yhouse/js/yhouse.js
@@ -0,0 +1,147 @@
+$(function(){
+	initEvents();
+});
+function initEvents(){
+	//footTop中，鼠标移入和移出时，字体颜色的改变；
+	changeFootTop();
+	//type="text"，当文本框获得和失去焦点时，根据现有文本框的内容判断是否改变文本框的内容
+	changeTextContent();
+	//mainLeft1 slide1中，幻灯片的实现；当鼠标移入和移出图片时，停止幻灯片的转换；当鼠标放到相应的小图标时，显示相应的图片
+	slide1MainLeft1();
+	//mainLeft2 slide2中，幻灯片的实现；当鼠标移入和移出图片时，停止幻灯片的转换；当鼠标放到相应的小图标时，显示相应的图片
+	slide2MainLeft();
+	//main1Right slide3中,幻灯片的实现；当鼠标移入和移出图片时，停止幻灯片的转换；
+	slide3MainRight();
+	//main2Left中，改变透明度
+	$(".main2LeftImgs span").css("opacity","0.5");
+
+	//main2Right中，选项卡
+	tabMain2Right();
+}
+//footTop中，鼠标移入和移出时，字体颜色的改变；
+function changeFootTop(){
+	$(".footMainT a").hover(function(){
+		$(this).css("color", "red");
+		$(this).find("span").css("color", "red");
+	},function(){
+		$(this).css("color", "rgb(204, 204, 204)");
+		$(this).find("span").css("color", "rgb(119, 119, 119)");
+	});
+}
+
+//type="text"，当文本框获得和失去焦点时，根据现有文本框的内容判断是否改变文本框的内容
+function changeTextContent(){
+	$("[type='text']").focus(function(){
+		//alert("d");
+		var val=$(this).val();
+		var txt=$(this).attr("name");
+		if (val==txt) {
+			$(this).val("");
+		};
+	}).blur(function(){
+		var val=$(this).val();
+		var txt=$(this).attr("name");
+		if (val=="") {
+			$(this).val(txt);
+		};
+	});
+}
+//mainLeft1Top中，简单的幻灯片
+var handle1;
+function slide1MainLeft1(){
+	handle1=setInterval("slide1()",2000);
+	slideOther("main1SlideUl","main1SlideNum",handle1,"slide1()",indexSlide1,1);
+}
+var indexSlide1=1;
+function slide1(){
+	if (indexSlide1==5) {
+		indexSlide1=1;
+		//$(".main1SlideUl").css("left","0px");
+	};
+	slideChangePicAndNum("main1SlideUl","main1SlideNum",indexSlide1,1);
+	indexSlide1++;
+}
+//mainLeft2中，幻灯片的实现；当鼠标移入和移出图片时，停止幻灯片的转换；当鼠标放到相应的小图标时，显示相应的图片
+var handle2;
+function slide2MainLeft(){
+	handle2=setInterval("slide2()",2000);
+	slideOther("main1Slide2Ul","main1Slide2Num",handle2,"slide2()",indexSlide2,2);
+}
+var indexSlide2=1;
+function slide2(){
+	if (indexSlide2==3) {
+		indexSlide2=1;
+	};
+	slideChangePicAndNum("main1Slide2Ul","main1Slide2Num",indexSlide2,2);
+	indexSlide2++;
+}
+
+//main1Right slide3中,幻灯片的实现；当鼠标移入和移出图片时，停止幻灯片的转换；
+var handle3;
+function slide3MainRight(){
+	handle3=setInterval("slide3()",2000);
+	$(".main1Right1Slide3Ul").hover(function(){
+		clearInterval(handle3);
+	},function(){
+		handle3=setInterval("slide3()",2000);
+	});
+}
+var indexSlide3=1;
+function slide3(){
+	if (indexSlide3==5) {
+		indexSlide3=1;
+	};
+	slideChangePicAndNum("main1Right1Slide3Ul","",indexSlide3,3);
+	indexSlide3++;
+}
+
+//当鼠标移入和移出图片时，停止幻灯片的转换；当鼠标放到相应的小图标时，显示相应的图片
+function slideOther(ulPicName,ulnumName,handle,slideFun,index,widthIndex){
+	$("."+ulPicName+" li").hover(function(){
+		clearInterval(handle);
+	},function(){
+		handle=setInterval(slideFun,2000);
+	});
+	$("."+ulnumName+" li").hover(function(){
+		clearInterval(handle);
+		var val=parseInt($(this).attr("value"));
+		index=val;
+		slideChangePicAndNum(ulPicName,ulnumName,index,widthIndex);
+	},function(){
+		handle=setInterval(slideFun,2000);
+	});
+}
+
+//slide,切换图片和改变图标
+function slideChangePicAndNum(ulPicName,ulnumName,index,widthIndex){
+	var width;
+	if (widthIndex==1) {
+		width=680;
+	}else{
+		if (widthIndex==2) {
+			width=320;
+		}else{
+			if (widthIndex==3) {
+				width=246;
+			}
+		}
+	}
+	$("."+ulPicName).animate({left:"-"+(index-1)*width+"px"},1000);
+	if (ulnumName!="") {
+		$("."+ulnumName+" li").css({"background-color":"rgb(255,255,255)","opacity":"0.5"});
+		$("."+ulnumName+" li:nth-child("+index+")").css({"background-color":"rgb(255,255,255)","opacity":"1"});
+	};	
+}
+
+//main2Right中，选项卡
+function tabMain2Right(){
+	$(".main2RightUl li").mouseover(function(){
+		$(".main2RightUl li").css("color","rgb(51,51,51)");
+		$(this).css("color","rgb(193, 39, 45)");
+		var val=$(this).attr("value");
+		$(".main2RightD").hide();
+		$(".main2RightD"+val).show();
+	});
+}
+
+
